3fe488e082be7b8c39a2b2e880784370e7e94bcb
Avan_41 <79739279+Avan-xli@users.noreply.github.com>
2021-12-27 12:48:06 +1300

    v1.5b

--- scripts/script.js
+++ scripts/script.js
@@ -52,14 +52,3 @@ if (localStorage.getItem('mode') == 'false') {
     lightModeEnable();
     console.log('light')
 }
-
-
-//date function
-var dateObj = new Date();
-var month = dateObj.getUTCMonth() + 1; //months from 1-12
-var day = dateObj.getUTCDate();
-var year = dateObj.getUTCFullYear();
-
-newdate = year + "/" + month + "/" + day;
-
-document.getElementById("current_date").innerHTML = year + "/" + month + "/" + day;
